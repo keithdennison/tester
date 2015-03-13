749e3cc199874142d3ecba2e13c8007a8e2830b2
Keith Dennison <dennison.keith@gmail.com>
2015-03-13 11:01:36 -0400

    Initial add of files.

---
+++ tester/tester.cpp
@@ -0,0 +1,16 @@
+// tester.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	wcout << "Starting program \"" << argv[0] << "\"" << endl;
+	string line;
+	getline(cin, line);
+
+	return 0;
+}
+
